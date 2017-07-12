6f4224369256290ef095d78b54c0843a5a7c1514
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-12 05:26:34 -0500

    image

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 'use strict';
 
-const express = require('express');
-const app = express();
+
 
 //aquí le decimos que use los archivos estáticos
 // y que se muestren al llamar a la ruta '/'
-app.use('/', express.static('public'));
+
 
 //aquí le decimos al framework desde ue puerto
 app.listen(3000, ()=>{
