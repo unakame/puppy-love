9aff822ebf7c930ff673cdc8d8abf019d0b112f8
Dana Franco <turquoise.ilg@hotmail.com>
2017-07-12 13:16:14 -0500

    changing

--- server.js
+++ server.js
@@ -1,12 +1,46 @@
-'use strict';
+var express = require("express");
+var api = require("./api");
+const app = express();
+
+app.get('/api/news/', function (req, res) {
+    let news = api.news();
+
+    news.then( (result) => {
+        console.log('Obteniendo noticias\nCantidad de Resultado:' +result.length);
+        res.status(200).json(result);
+    });
+});
+
+app.get('/api/news/:news_id', function (req, res) {
+    let news = api.news(req.params.news_id);
+
+    news.then( (result) => {
+        console.log('Obteniendo noticia con id: '+ req.params.news_id + '\nResultado title:' +result.title);
+        res.status(200).json(result);
+    });
+});
+
+app.get('/api/categories/', function (req, res) {
+    let categories = api.categories();
+
+    categories.then( (result) => {
+        console.log('Obteniendo categorias\nCantidad de Resultados:' +result.length);
+        res.status(200).json(result);
+    });
+});
+
+app.get('/api/categories/:category_id', function (req, res) {
+    let categories = api.categories(req.params.category_id);
+
+    categories.then( (result) => {
+        console.log('Obteniendo categorias con id: '+ req.params.category_id + '\nResultado title:' +result.title);
+        res.status(200).json(result);
+    });
+});
+
+app.use('/', express.static('public'));
 
 
-
-//aquí le decimos que use los archivos estáticos
-// y que se muestren al llamar a la ruta '/'
-
-
-//aquí le decimos al framework desde ue puerto
-app.listen(3000, ()=>{
-  console.log("listening on 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Iniciando en puerto 3000");
 });
